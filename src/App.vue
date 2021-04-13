2c1509ff0a87edd775173154b4d8d1ca44e7b950
Szymon Wójcik <wojcik.szymon1@gmail.com>
2021-04-13 12:46:53 +0200

    typography

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
+  <TheNavigation></TheNavigation>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Test</h1>
+  <p>Elo ziomy</p>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheNavigation from "@/components/layout/TheNavigation";
 
 export default {
   name: 'App',
   components: {
+    TheNavigation,
     HelloWorld
   }
 }
@@ -16,4 +21,5 @@ export default {
 
 <style lang="scss">
 @import "styles/base/_base.scss";
+@import "styles/base/typography";
 </style>
---
+++ src/components/layout/TheNavigation.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="nav">
+    <h2>DESITE</h2>
+
+    <nav>
+      <h3>test</h3>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheNavigation"
+}
+</script>
+
+<style lang="scss" scoped>
+  .nav {
+    display: flex;
+    background-color: orangered;
+    width: 90%;
+    margin: 0 auto;
+
+    h2 {
+      font-family: Montserrat, sans-serif;
+      font-weight: 800;
+      font-size: 1.4rem;
+    }
+
+    nav {
+      display: flex;
+      flex: 1;
+      justify-content: end;
+      background-color: green;
+
+      h3 {
+        font-family: Mulish, sans-serif;
+        font-weight: 300;
+      }
+    }
+
+
+  }
+</style>
